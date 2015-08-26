ce10090bfb9a6a8c139014d53f4d1e9d85b28a34
Vadim Rastyagaev <rastyagaev@gmail.com>
2015-08-26 13:10:01 +0200

    moved repo & settings

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+Setup:
+
 1. Create an heroku application.
 2. Store secrets to heroku applicaiton settings with prefix 'MARKIN_'.
 3. Check ansible/group_vars/all and capistrano/config/deploy.rb for settings.
@@ -5,3 +7,9 @@
 4. Run `bundle install --gemfile capistrano/Gemfile`.
 5. Run `ansible-galaxy install -r ansible/requirements.txt --force`.
 6. Run `ansible-playbook -i ansible/production.ini ansible/application.yml`.
+
+Deploy:
+
+1. Enter to `capistrano` folder.
+2. Run `bundle install`
+3. Run `bundle exec cap production deploy`
--- capistrano/config/deploy.rb
+++ capistrano/config/deploy.rb
@@ -1,13 +1,13 @@
 lock '3.2.1'
 
 set :application, 'markin'
-set :repo_url, 'git@github.com:f-s-v/markin-application.git'
+set :repo_url, 'git@github.com:vladimirmarkin/markin-application.git'
 set :user, :web
 set :deploy_to, "/home/#{fetch(:user)}/applications/#{fetch(:application)}"
 set :linked_dirs, %w{log tmp/pids tmp/cache tmp/sockets vendor/bower_components}
 set :linked_files, %w{.env}
 set :ssh_options, { forward_agent: true }
-set :secrets_app_name, 'f-s-v-secrets'
+set :secrets_app_name, 'vladimirmarkin-secrets'
 set :secrets_app_pattern, 'MARKIN'
 set :services, [
   {name: fetch(:application), roles: :app, sudo: true}
